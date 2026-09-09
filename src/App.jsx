1b705485fc1b89ecda56403e5df8aaaa2d869da9
pakkawatmai-140759 <pakkawatmai@gmail.com>
2026-09-09 14:01:14 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -253,7 +253,6 @@ export default function App() {
   const [schedules, setSchedules] = useState([]);
   
   const [isBooking, setIsBooking] = useState(false);
-  const [sideNote, setSideNote] = useState('');
   
   const getInitialBookingData = () => ({ 
     id: null, date: '', time: '', type: 'trial', title: '', detail: '', clientId: '', partId: '', machine: '', requester: '', location: '',
